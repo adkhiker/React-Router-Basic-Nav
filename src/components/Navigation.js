345ed16e5ed427dac1788f6734bcbc1f382872f3
adkhiker <poirier.greg@gmail.com>
2019-10-02 21:27:40 -0400

    MVP: Nav links finished

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Navigation = () => {
@@ -6,14 +6,17 @@ const Navigation = () => {
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
+
         <div>
-          <a href="">Home</a>
+          <Link to="/">Home</Link>
         </div>
+
         <div>
-          <a href="">About</a>
+          <Link to="about">About</Link>
         </div>
+
         <div>
-          <a href="">Contact</a>
+          <Link to="contact">Contact</Link>
         </div>
       </div>
     </div>
